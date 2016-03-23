d008fe87b286cd2752ab22eaf67c75c435e9cece
rawlingsj <rawlingsj80@gmail.com>
2016-03-23 17:10:06 +0000

    change the default value so we dont mess up the forge use of $JENKINS_WORKFLOW_GIT_REPOSITORY

--- postStart.sh
+++ postStart.sh
@@ -6,7 +6,7 @@
 
 # Initialise the workflow global git repo with reusable scripts
 # Default library can be overritten using $JENKINS_WORKFLOW_GIT_REPOSITORY env var
-if [ ! -z "$JENKINS_WORKFLOW_GIT_REPOSITORY" ] && [ "$JENKINS_WORKFLOW_GIT_REPOSITORY" != "default" ]; then
+if [ ! -z "$JENKINS_WORKFLOW_GIT_REPOSITORY" ] && [ "$JENKINS_WORKFLOW_GIT_REPOSITORY" != "https://github.com/fabric8io/jenkins-workflow-library.git" ]; then
   rm -rf /root/repositoryscripts
   git clone "$JENKINS_WORKFLOW_GIT_REPOSITORY" /root/repositoryscripts
 fi
